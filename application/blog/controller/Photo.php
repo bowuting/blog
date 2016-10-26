035fdae33ebce34f1bb60ccf3ef36c426e5363f9
bowuting <bowuting@gmail.com>
2016-10-26 09:11:26 +0800

    照片上传

--- application/blog/controller/Photo.php
+++ application/blog/controller/Photo.php
@@ -5,16 +5,16 @@ use app\blog\model\PostsModel;  // 文章模型
 use app\blog\model\LoginModel;  // 登录模型
 use app\blog\model\PhotoModel;
 use think\Db;
-require '../vendor/qiniu/autoload.php';
-use Qiniu\Auth;
-use Qiniu\Storage\UploadManager;
+
 class Photo extends Controller
 {
     public function index(){
 
 
-            $data = Db::table('blog_photo_cat')->column('photo_cat_name');
+            // $data = Db::table('blog_photo_cat')->column('photo_cat_name');
+            $data = Db::table('blog_photo_cat')->select();
             // var_dump($data);
+            // exit();
             $this->assign('data',$data);
 
 
@@ -35,9 +35,21 @@ class Photo extends Controller
     public function add_cat()
     {
         if (LoginModel::isLogin()) {
-            if (Db::table('blog_photo_cat')->insert($_POST)) {
-                return $this->success("新建成功 ",url('index'));
+
+            $file = request()->file('cat_img');
+            if ($file) {
+                $qiniuurl = PhotoModel::upload($file);
+                $_POST['photo_cat_url'] = $qiniuurl;
+
+
+                if (Db::table('blog_photo_cat')->insert($_POST)) {
+
+                    return $this->success("新建成功 ",url('index'));
+                }
+            } else {
+                return $this->error('您没有选择文件 不能新建相册');
             }
+
 
             // print_r($_POST);
         } else {
@@ -79,52 +91,29 @@ class Photo extends Controller
             // print_r($_POST['cat']);
             // exit();
             $file = request()->file('image');
+            if ($file) {
+                $qiniuurl = PhotoModel::upload($file);
+            } else {
+                return $this->error('您没有选择照片 不能添加照片');
+            }
 
-            $path = ROOT_PATH . 'public' . DS . 'uploads/';
 
-            $info = $file->rule('md5')->move($path);
-            if($info){
-
-                    $accessKey = '3qi3glFmW7EAyOntSc6E2TXF2pNxHw4QMT_8VDik';
-                    $secretKey = 'mS4Zp_UCgNWeqBme3zscUVPlbift44GNsSinMQBm';
-
-                    $auth = new Auth($accessKey, $secretKey);
-                    $bucket = 'qingdao';
-
-                    $token = $auth->uploadToken($bucket);
-                    $filePath = $path . $info->getSaveName();
-                    $key = $info->getSaveName();
-
-                    $uploadMgr = new UploadManager();
-                    list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
-                    if ($err == null) {
-                        // var_dump($ret);
-                        $qiniu = "http://file.bowuting.com/";
-                        $qiniu = $qiniu .  $info->getSaveName();
-                        // echo $qiniu;
-                        //
-
-                        $photo = new PhotoModel;
-                        $photo->photo_cat = $_POST['cat'];
-                        $photo->photo_url = $qiniu;
-                        $photo->photo_name = $_POST['photo_name'];
-                        $photo->photo_content = $_POST['photo_content'];
-                        if ($photo->save()) {
-                            return $this->success('上传成功',url('photo?cat=') .$_POST['cat'] );
-                        }
-
-                    } else {
-                        var_dump($err);
-                    }
+            if ($qiniuurl) {
+                $photo = new PhotoModel;
+                $photo->photo_cat = $_POST['cat'];
+                $photo->photo_url = $qiniuurl;
+                $photo->photo_name = $_POST['photo_name'];
+                $photo->photo_content = $_POST['photo_content'];
+                if ($photo->save()) {
+                    return $this->success('上传成功',url('photo?cat=') .$_POST['cat'] );
                 } else {
-                    // 上传失败获取错误信息
                     echo $file->getError();
                 }
             } else {
                 return $this->error('您没有登录 不能添加照片');
             }
     }
-
+    }
 }
 
 ?>
--- application/blog/model/PhotoModel.php
+++ application/blog/model/PhotoModel.php
@@ -4,6 +4,10 @@ namespace app\blog\model;
 use think\Model;    // 使用前进行声明
 use traits\model\SoftDelete;
 
+require '../vendor/qiniu/autoload.php';
+use Qiniu\Auth;
+use Qiniu\Storage\UploadManager;
+
 class PhotoModel extends Model
 {
 
@@ -12,6 +16,37 @@ class PhotoModel extends Model
     protected static $deleteTime = 'delete_time';
 
     protected $table = "blog_photo";
+
+    static public function upload($file){
+        $path = ROOT_PATH . 'public' . DS . 'uploads/';
+            $info = $file->rule('md5')->move($path);
+
+        if($info){
+
+                $accessKey = '3qi3glFmW7EAyOntSc6E2TXF2pNxHw4QMT_8VDik';
+                $secretKey = 'mS4Zp_UCgNWeqBme3zscUVPlbift44GNsSinMQBm';
+
+                $auth = new Auth($accessKey, $secretKey);
+                $bucket = 'qingdao';
+
+                $token = $auth->uploadToken($bucket);
+                $filePath = $path . $info->getSaveName();
+                $key = $info->getSaveName();
+
+                $uploadMgr = new UploadManager();
+                list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
+                if ($err == null) {
+                    $qiniuurl = "http://file.bowuting.com/";
+                    $qiniuurl = $qiniuurl .  $info->getSaveName();
+                    return $qiniuurl;
+                } else {
+                    return($err);
+                }
+            } else {
+                // 上传失败获取错误信息
+                return  $file->getError();
+            }
+    }
 }
 
 
